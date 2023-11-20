7c22676bbb833bfc70f29846b080023a66d37e06
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-20 16:56:05 +0900

    fix: fix server error stacktrace

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { importDevServerPlugin } from "@hiogawa/vite-import-dev-server";
 
 export default defineConfig({
   clearScreen: false,
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [importDevServerPlugin(), remix(), tsconfigPaths()],
 });
